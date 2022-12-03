ec2a9e940165717922ed5df37c268a9a5450b850
veronicadiazr7 <veronica.ddiaz7@gmail.com>
2022-12-03 12:25:53 -0400

    first API commit

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const app = express();
+const fs = require("fs");
+
+app.get("/getSources", (req, res) => {
+  fs.readFile(__dirname + "/" + "sources.json", "utf8", (err, data) => {
+    // console.log(data);
+    res.send(data);
+  });
+});
+
+let server = app.listen(8000, () => {
+  const host = server.address().address;
+  const port = server.address().port;
+  console.log("Server is listening at http://%s:%s", host, port);
+});
+
+let addSource = {
+  Source12: {
+    id: 12,
+    name: "SolarAid",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/SolarAid_Logo.png/1200px-SolarAid_Logo.png",
+    url: "https://solaraid.org",
+    type: "Organization",
+  },
+};
+
+app.post("/addSource", (req, res) => {
+  fs.readFile(__dirname + "/" + "sources.json", "utf8", (err, data) => {
+    data = JSON.parse(data);
+    data["Source12"] = addSource["Source12"];
+    console.log(data);
+    res.end(JSON.stringify(data));
+  });
+});
+
+app.get("/:id", (req, res) => {
+  fs.readFile(__dirname + "/" + "sources.json", "utf8", (err, data) => {
+    var sources = JSON.parse(data);
+    var source = sources["source" + req.params.id];
+    console.log(source);
+    res.end(JSON.stringify(source));
+  });
+});
+
+let id = 3;
+app.delete("/deleteSource/", (req, res) => {
+  fs.readFile(__dirname + "/" + "sources.json", "utf8", (err, data) => {
+    data = JSON.parse(data);
+    delete data["source" + 3];
+
+    console.log(data);
+    res.end(JSON.stringify(data));
+  });
+});
